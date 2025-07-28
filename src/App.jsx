71774b48ab9de31a429f98ab8afddc93f8a92482
João Lucas Gifoni <jl109160@gmail.com>
2025-07-28 01:11:00 +0000

    Git inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import supabase from "./supabase-client";
+
+
+function App() {
+
+  return (
+    <div>
+      {""}
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App
